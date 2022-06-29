3547f88118628678340c7e3813b0f04e405dc49f
Giovanni Lopes <giovanniclopes@gmail.com>
2022-06-29 18:06:58 -0300

    media query added

--- tailwind.config.js
+++ tailwind.config.js
@@ -4,6 +4,16 @@ module.exports = {
     './src/**/*.tsx'
   ],
   theme: {
+    screens: {
+      'tablet': '640px',
+      // => @media (min-width: 640px) { ... }
+
+      'laptop': '1024px',
+      // => @media (min-width: 1024px) { ... }
+
+      'desktop': '1280px',
+      // => @media (min-width: 1280px) { ... }
+    },
     extend: {
       backgroundImage: {
         blur: 'url(/src/assets/blur-background.png)'
