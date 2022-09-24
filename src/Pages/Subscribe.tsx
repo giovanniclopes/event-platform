020b547da3aada1acb2d9636fffd3f4323a53aae
Giovanni Lopes <giovanniclopes@gmail.com>
2022-09-24 19:43:37 -0300

    Update Subscribe.tsx

--- src/Pages/Subscribe.tsx
+++ src/Pages/Subscribe.tsx
@@ -26,39 +26,38 @@ export function Subscribe() {
     }
 
     return (
-        <div className="min-h-screen bg-blur bg-cover bg-no-repeat flex flex-col items-center">
+        <div className="flex flex-col items-center min-h-screen bg-no-repeat bg-cover bg-blur">
             <div className="w-full max-w-[1100px] flex items-center justify-between mt-20 mx-auto sm:flex-col gap-8">
                 <div className="max-w-[640px] sm:max-w-[300px]">
                     <Logo />
 
                     <h1 className="mt-8 text-[2.5rem] leading-tight sm:text-2xl">
-                        Construa uma <strong className="text-blue-500">aplicação completa</strong>, do zero, com <strong className="
-                        text-blue-500">React JS</strong>
+                        Construa uma <strong className="text-blue-500">aplicação completa</strong>, do zero, com <strong className="text-blue-500 ">React JS</strong>
                     </h1>
-                    <p className="mt-4 text-gray-200 leading-relaxed">
+                    <p className="mt-4 leading-relaxed text-gray-200">
                         Em apenas uma semana você vai dominar na prática uma das tecnologias mais utilizadas e com alta demanda para acessar as melhores oportunidades do mercado.
                     </p>
                 </div>
 
                 <div className="p-8 bg-gray-700 border border-gray-500 rounded sm:mb-6">
-                    <strong className="text-2xl mb-6 block">Inscreva-se gratuitamente</strong>
+                    <strong className="block mb-6 text-2xl">Inscreva-se gratuitamente</strong>
 
-                    <form onSubmit={handleSubscribe} className="flex flex-col gap-2 w-full">
+                    <form onSubmit={handleSubscribe} className="flex flex-col w-full gap-2">
                         <input 
-                        className="bg-gray-900 rounded px-5 h-14"
+                        className="px-5 bg-gray-900 rounded h-14"
                         type="text" 
                         placeholder="Seu nome completo" 
                         onChange={event => setName(event.target.value)}
                         />
                         <input 
-                        className="bg-gray-900 rounded px-5 h-14"
+                        className="px-5 bg-gray-900 rounded h-14"
                         type="email"
                         placeholder="Digite seu email" 
                         onChange={event => setEmail(event.target.value)}
                         />
 
                         <button 
-                        className="mt-4 bg-green-500 uppercase py-4 rounded font-bold text-sm hover:bg-green-700 transition-colors disabled:opacity-50"
+                        className="py-4 mt-4 text-sm font-bold uppercase transition-colors bg-green-500 rounded hover:bg-green-700 disabled:opacity-50"
                         disabled={loading}
                         type="submit">
                             Garantir minha vaga
