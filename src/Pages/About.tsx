b3eabb09c125c3a445f2585d8671cc4c418f0089
Giovanni Lopes <giovanniclopes@gmail.com>
2023-09-05 23:15:57 -0300

    Update About.tsx

--- src/Pages/About.tsx
+++ src/Pages/About.tsx
@@ -43,7 +43,9 @@ export default function About() {
 
       <div className="flex flex-col items-center justify-center gap-16 my-28">
         <div className="flex flex-col items-center justify-center gap-4">
-          <h1 className="text-4xl font-black uppercase">Quem <span className="text-product-pink">somos</span></h1>
+          <h1 className="text-4xl font-black uppercase">
+            Quem <span className="text-product-pink">somos</span>
+          </h1>
         </div>
         <div className="flex flex-row items-start justify-start gap-12 px-12 mbl:flex mbl:flex-col">
           <div className="w-2/3 mbl:w-full">
@@ -54,12 +56,31 @@ export default function About() {
             />
           </div>
           <div className="flex flex-col gap-12 w-1/2 mbl:w-full">
-            <p className="text-xl leading-relaxed">
-              Somos um grupo de amigos que tivemos a ideia do IntelliOn na nossa
-              última feira de ciências na nossa escola no Ensino Fundamental, e
-              então nos juntamos e levamos o projeto para frente, e agora somos
-              reconhecidos por várias empresas pelas nossas aulas flexíveis e
-              muito explicativas.
+            <p className="text-xl leading-relaxed indent-3">
+              Conheça a <span className="font-bold">IntelliOn</span>! Somos um
+              grupo de amigos que teve essa ideia incrível durante a feira de
+              ciências da escola. Agarramos a oportunidade e agora, aqui estamos
+              nós, sendo reconhecidos por várias empresas devido às nossas aulas
+              super flexíveis e de fácil compreensão. Imagine aprender a usar a
+              internet de forma tranquila, sem preocupações! Nós desenvolvemos
+              aulas e textos que ensinam como navegar na internet com total
+              segurança. O melhor de tudo? As nossas aulas são para todos -
+              idosos, crianças e pais podem aproveitar.
+              <br />
+              <br />
+              <p className="text-xl leading-relaxed indent-3">
+                O que começou como uma ideia entre amigos, agora está ganhando
+                reconhecimento pelo nosso trabalho sólido. As empresas estão
+                percebendo a qualidade do que oferecemos. Com a IntelliOn, você
+                aprenderá as práticas para navegar na internet com confiança e
+                manter-se seguro. É isso, a IntelliOn está crescendo e ajudando
+                pessoas a terem uma experiência online tranquila. Junte-se a nós
+                nessa jornada de aprendizado e segurança na internet. <br />
+                <span className="font-semibold text-product-pink">
+                  Boas-vindas ao mundo IntelliOn
+                </span>
+                , onde a internet se torna ainda mais acessível!
+              </p>
             </p>
             <Link className="w-max" to="/contact">
               <button className=" flex items-center justify-center py-3 px-8 font-semibold text-product-pink border border-product-pink rounded transition-color hover:text-white hover:bg-product-pink">
@@ -72,18 +93,9 @@ export default function About() {
 
       <div className="flex items-center justify-center">
         <div className="grid grid-cols-3 items-center justify-center gap-24 mbl:flex mbl:flex-col">
-          <Infos
-            title="Alunos cadastrados"
-            text="+290"
-          />
-          <Infos
-            title="Oportunidades de emprego"
-            text="+63"
-          />
-          <Infos
-            title="Empresas parceiras"
-            text="+6"
-          />
+          <Infos title="Alunos cadastrados" text="+290" />
+          <Infos title="Oportunidades de emprego" text="+63" />
+          <Infos title="Empresas parceiras" text="+6" />
         </div>
       </div>
     </main>
