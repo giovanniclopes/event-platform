3f76824d2537cfc7348ba8c8ae38c89735658cd9
Giovanni Lopes <giovanniclopes@gmail.com>
2023-09-24 05:19:03 -0300

    Update Video.tsx

--- src/components/Video.tsx
+++ src/components/Video.tsx
@@ -1,123 +1,147 @@
 import { DefaultUi, Player, Youtube } from "@vime/react";
-import { CaretRight, DiscordLogo, FileArrowDown, Lightning, Spinner } from "phosphor-react";
+import {
+  CaretRight,
+  DiscordLogo,
+  FileArrowDown,
+  Lightning,
+  Spinner,
+} from "phosphor-react";
 
-import '@vime/core/themes/default.css';
+import "@vime/core/themes/default.css";
 import { useGetLessonBySlugQuery } from "../graphql/generated";
 import { Link } from "react-router-dom";
 
 interface VideoProps {
-    lessonSlug: string;
+  lessonSlug: string;
 }
 
 export function Video(props: VideoProps) {
-    const { data } = useGetLessonBySlugQuery({
-        variables: {
-          slug: props.lessonSlug,
-        },
-        fetchPolicy: 'no-cache'
-    })
+  const { data } = useGetLessonBySlugQuery({
+    variables: {
+      slug: props.lessonSlug,
+    },
+    fetchPolicy: "no-cache",
+  });
 
-    if (!data || !data.lesson) {
-        return (
-            <div className="flex-1 flex flex-col justify-center items-center gap-2 text-gray-400 bg-reactIcon bg-center bg-no-repeat bg-contain">
-                 <Spinner size={64}>
-                    <animateTransform
-                        attributeName="transform"
-                        attributeType="XML"
-                        type="rotate"
-                        dur="5s"
-                        from="0 0 0"
-                        to="360 0 0"
-                        repeatCount="indefinite"
-                    />
-                </Spinner>
-                <p className="text-lg">Carregando...</p>
-            </div>
-        )
-    }
-
+  if (!data || !data.lesson) {
     return (
-        <div className="flex-1 mt-3">
-            <div className="bg-black flex justify-center">
-                <div className="h-full w-full max-w-[1100px] max-h-[60vh] aspect-video">
-                    <Player controls>
-                        <Youtube videoId={data.lesson.videoId} />
-                        <DefaultUi />
-                    </Player>
-                </div>
-            </div>
+      <div className="flex-1 flex flex-col justify-center items-center gap-2 text-gray-400 bg-reactIcon bg-center bg-no-repeat bg-contain">
+        <Spinner size={64}>
+          <animateTransform
+            attributeName="transform"
+            attributeType="XML"
+            type="rotate"
+            dur="5s"
+            from="0 0 0"
+            to="360 0 0"
+            repeatCount="indefinite"
+          />
+        </Spinner>
+        <p className="text-lg">Carregando...</p>
+      </div>
+    );
+  }
 
-            <div className="p-8 max-w-[1100px] mx-auto">
-                <div className="flex items-start gap-16 mbl:block">
-                    <div className="flex-1">
-                        <h1 className="text-2xl font-bold">
-                            {data.lesson.title}
-                        </h1>
-                        <p className="mt-4 text-gray-200 leading-relaxed">
-                            {data.lesson.description}
-                        </p>
-
-                        {data.lesson.teacher && (
-                            <div className="flex items-center gap-4 mt-6 mbl:mt-10">
-                                <img
-                                className="h-16 w-16 rounded-full border-2 border-product-purple" 
-                                src={data.lesson.teacher.avatarURL}
-                                alt="Avatar"
-                                />
-
-                                <div className="leading-relaxed">
-                                    <strong className="font-bold text-2xl block">{data.lesson.teacher.name}</strong>
-                                    <span className="text-gray-200 text-sm block">{data.lesson.teacher.bio}</span>
-                                </div>
-                            </div>
-                        )}
-                    </div>
-
-                    <div className="flex flex-col gap-4 mbl:mt-10">
-                        <a href="" className="p-4 text-sm bg-product-purple flex items-center rounded font-bold uppercase gap-2 justify-center hover:bg-product-darkPurple transition-colors">
-                            <DiscordLogo size={24} />
-                            Comunidade do Discord
-                        </a>
-
-                        <a href="" className="p-4 text-sm border border-product-lightPink text-product-lightPink flex items-center rounded font-bold uppercase gap-2 justify-center hover:bg-product-lightPink hover:text-gray-900 transition-colors">
-                            <Lightning size={24} />
-                            Acesse o desafio
-                        </a>
-                    </div>
-                </div>
-
-                <div className="gap-8 mt-20 grid grid-cols-2 mbl:grid-cols-1">
-                    <Link to={''} target="_blank" download={data.lesson.complementaryMaterial} className="bg-gray-700 rounded overflow-hidden flex items-stretch gap-6 hover:bg-gray-600 transition-colors">
-                        <div className="bg-product-purple h-full p-6 flex items-center">
-                            <FileArrowDown size={40} />
-                        </div>
-                        <div className="py-6 leading-relaxed">
-                            <strong className="text-2xl">Material complementar</strong>
-                            <p className="text-sm text-gray-200 mt-2">
-                                Acesse o material complementar para acelerar o seu desenvolvimento
-                            </p>
-                        </div>
-                        <div className="h-full p-6 flex items-center">
-                            <CaretRight size={24} />
-                        </div>
-                    </Link>
-
-                    <a href="" className="bg-gray-700 rounded overflow-hidden flex items-stretch gap-6 hover:bg-gray-600 transition-colors">
-                        <div className="bg-product-purple h-full p-6 flex items-center">
-                            <FileArrowDown size={40} />
-                        </div>
-                        <div className="py-6 leading-relaxed">
-                            <strong className="text-2xl">Wallpapers exclusivos</strong>
-                            <p className="text-sm text-gray-200 mt-2">
-                                Baixe wallpapers exclusivos do Ignite Lab e personalize a sua máquina
-                            </p>
-                        </div>
-                        <div className="h-full p-6 flex items-center">
-                            <CaretRight size={24} />
-                        </div>
-                    </a>
-                </div>
-            </div>
+  return (
+    <div className="flex-1 mt-3">
+      <div className="bg-black flex justify-center">
+        <div className="h-full w-full max-w-[1100px] max-h-[60vh] aspect-video">
+          <Player controls>
+            <Youtube videoId={data.lesson.videoId} />
+            <DefaultUi />
+          </Player>
         </div>
-    )
+      </div>
+
+      <div className="p-8 max-w-[1100px] mx-auto">
+        <div className="flex items-start gap-16 mbl:block">
+          <div className="flex-1">
+            <h1 className="text-2xl font-bold">{data.lesson.title}</h1>
+            <p className="mt-4 text-gray-200 leading-relaxed">
+              {data.lesson.description}
+            </p>
+
+            {data.lesson.teacher && (
+              <div className="flex items-center gap-4 mt-6 mbl:mt-10">
+                <img
+                  className="h-16 w-16 rounded-full border-2 border-product-purple"
+                  src={data.lesson.teacher.avatarURL}
+                  alt="Avatar"
+                />
+
+                <div className="leading-relaxed">
+                  <strong className="font-bold text-2xl block">
+                    {data.lesson.teacher.name}
+                  </strong>
+                  <span className="text-gray-200 text-sm block">
+                    {data.lesson.teacher.bio}
+                  </span>
+                </div>
+              </div>
+            )}
+          </div>
+
+          <div className="flex flex-col gap-4 mbl:mt-10">
+            <a
+              href=""
+              className="p-4 text-sm bg-product-purple flex items-center rounded font-bold uppercase gap-2 justify-center hover:bg-product-darkPurple transition-colors"
+            >
+              <DiscordLogo size={24} />
+              Comunidade do Discord
+            </a>
+
+            <a
+              href=""
+              className="p-4 text-sm border border-product-lightPink text-product-lightPink flex items-center rounded font-bold uppercase gap-2 justify-center hover:bg-product-lightPink hover:text-gray-900 transition-colors"
+            >
+              <Lightning size={24} />
+              Acesse o desafio
+            </a>
+          </div>
+        </div>
+
+        <div className="gap-8 mt-20 grid grid-cols-2 mbl:grid-cols-1">
+          <a
+            href={data.lesson.complementaryMaterial[0]?.url}
+            target="_blank"
+            download
+            className="bg-gray-700 rounded overflow-hidden flex items-stretch gap-6 hover:bg-gray-600 transition-colors"
+          >
+            <div className="bg-product-purple h-full p-6 flex items-center">
+              <FileArrowDown size={40} />
+            </div>
+            <div className="py-6 leading-relaxed">
+              <strong className="text-2xl">Material complementar</strong>
+              <p className="text-sm text-gray-200 mt-2">
+                Acesse o material complementar para acelerar o seu
+                desenvolvimento
+              </p>
+            </div>
+            <div className="h-full p-6 flex items-center">
+              <CaretRight size={24} />
+            </div>
+          </a>
+
+          <a
+            href=""
+            className="bg-gray-700 rounded overflow-hidden flex items-stretch gap-6 hover:bg-gray-600 transition-colors"
+          >
+            <div className="bg-product-purple h-full p-6 flex items-center">
+              <FileArrowDown size={40} />
+            </div>
+            <div className="py-6 leading-relaxed">
+              <strong className="text-2xl">Wallpapers exclusivos</strong>
+              <p className="text-sm text-gray-200 mt-2">
+                Baixe wallpapers exclusivos do Ignite Lab e personalize a sua
+                máquina
+              </p>
+            </div>
+            <div className="h-full p-6 flex items-center">
+              <CaretRight size={24} />
+            </div>
+          </a>
+        </div>
+      </div>
+    </div>
+  );
 }
