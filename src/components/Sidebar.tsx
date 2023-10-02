fc5bb7b552ea98226134bdc7ed262522c9d7fd2e
Giovanni Lopes <giovanniclopes@gmail.com>
2023-10-02 14:38:07 -0300

    Update Sidebar.tsx

--- src/components/Sidebar.tsx
+++ src/components/Sidebar.tsx
@@ -1,32 +1,28 @@
-import { gql, useQuery } from "@apollo/client";
 import { useGetLessonsQuery } from "../graphql/generated";
-import { Footer } from "./Footer";
 import { Lesson } from "./Lesson";
 
 export function Sidebar() {
-    const { data } = useGetLessonsQuery()
+  const { data } = useGetLessonsQuery();
 
-    
+  return (
+    <aside className="w-[348px] bg-[#121214] p-6 rounded mbl:w-[91.5vw] m-3">
+      <span className="font-bold text-2xl pb-6 mb-6 border-b border-gray-500 block">
+        Cronograma de aulas
+      </span>
 
-    return (
-      <aside className="w-[348px] bg-[#121214] p-6 rounded mbl:w-[91.5vw] m-3">
-        <span className="font-bold text-2xl pb-6 mb-6 border-b border-gray-500 block">
-          Cronograma de aulas
-        </span>
-
-        <div className="flex flex-col gap-8">
-          {data?.lessons.map((lesson) => {
-            return (
-              <Lesson
-                key={lesson.id}
-                title={lesson.title}
-                slug={lesson.slug}
-                availableAt={new Date(lesson.availableAt)}
-                type={lesson.lessonType}
-              />
-            );
-          })}
-        </div>
-      </aside>
-    );
+      <div className="flex flex-col gap-8">
+        {data?.lessons.map((lesson) => {
+          return (
+            <Lesson
+              key={lesson.id}
+              title={lesson.title}
+              slug={lesson.slug}
+              availableAt={new Date(lesson.availableAt)}
+              type={lesson.lessonType}
+            />
+          );
+        })}
+      </div>
+    </aside>
+  );
 }
